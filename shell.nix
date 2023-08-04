e3d92edd0f32dc55a4e7376c15e941ae066b4705
Michael Fliegner <12422482+michaelfliegner@users.noreply.github.com>
2023-08-04 12:26:04 +0200

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -6,6 +6,9 @@ mkShell {
   # Package names can be found via https://search.nixos.org/packages
   nativeBuildInputs = [
     direnv
+    postgresql
+    ghc
+    cabal2nix
   ];
 
   NIX_ENFORCE_PURITY = true;
