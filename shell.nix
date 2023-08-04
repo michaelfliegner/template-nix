62b6da5ff25e572c67dfa63597a74a507595a61c
Kaeptenblaubaer <michelkannfliegen@gmail.com>
2023-08-04 17:57:10 +0000

    postgres successful connect

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@
 with import <nixpkgs> { };
 
 mkShell {
-
+  LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
+  
   # Package names can be found via https://search.nixos.org/packages
   nativeBuildInputs = [
     direnv
@@ -16,5 +17,9 @@ mkShell {
   NIX_ENFORCE_PURITY = true;
 
   shellHook = ''
-  '';
+    exportLOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+    export LANG="en_US.UTF-8"
+    LC_ALL="en_US.UTF-8"
+    LC_CTYPE="en_US.UTF-8"
+ '';
 }
